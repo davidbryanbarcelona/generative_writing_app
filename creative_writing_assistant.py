1e664536e0be83f06a0d16f5535bde48b9d93b9f
David Bryan Barcelona <99592635+davidbryanbarcelona@users.noreply.github.com>
2024-05-08 23:25:53 +0800

    Update creative_writing_assistant.py

--- creative_writing_assistant.py
+++ creative_writing_assistant.py
@@ -21,7 +21,16 @@ async def generate_response(question, context):
 
 
 async def app():
-  st.title("OpenAI Text Generation App")
+  st.title("Creative Writing Assistant App")
+    
+    text = """David Bryan S. Barcelona\n\n
+    CCS 229 - Intelligent Systems
+    College of Information and Communications Technology
+    West Visayas State University"""
+    st.text(text)
+
+    # st.image('')
+
 
   # Text area input for the context
   context = st.text_area("Enter the context:")
