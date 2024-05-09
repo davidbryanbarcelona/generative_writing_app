d3e152bf95330149a1a8c59213dce247ac6c7ae4
David Bryan Barcelona <99592635+davidbryanbarcelona@users.noreply.github.com>
2024-05-10 00:38:45 +0800

    Update creative_writing_assistant.py

--- creative_writing_assistant.py
+++ creative_writing_assistant.py
@@ -10,14 +10,13 @@ client = AsyncOpenAI(
     api_key=st.secrets["API_key"],
 )
 
-async def generate_response(question, genre, prompt):
+async def generate_response(question, genre):
   model = "gpt-3.5-turbo"
   #model - "gpt-3.5-turbo"
 
   completion = await client.chat.completions.create(model=model, 
       messages=[{"role": "user", "content": question}, 
-                {"role": "system", "content": genre},
-                {"role": "user", "content": prompt}])
+                {"role": "system", "content": genre})
   return completion.choices[0].message.content
 
 
@@ -37,31 +36,17 @@ async def app():
   question = st.text_input("Hi, I'm your Creative Writing Assitant! How can I help your literary?")
    
   # User selects the type of literature to generate
-  genre = st.selectbox(
-      "Select the type of literature you want to generate:",
-      ["Poetry", "Drama", "Fiction", "Non-Fiction", "Folklore"]
-  )
-
-  prompt = ""
-  if genre == "Poetry":
-      prompt = "Write a poetry about"
-  elif genre == "Drama":
-      prompt = "Write a drama story about"
-  elif genre == "Fiction":
-      prompt = "Write a fictional story about"
-  elif genre == "Non-Fiction":
-      prompt = "Write a non-fictional story about"
-  elif genre == "Folklore":
-      prompt = "Write a folklore story about"
-
+    # Text input for user question
+  genre = st.text_input("Enter the genre of the literary you want to write:")
+  
   # Button to generate response
   if st.button("Generate Response"):
       if question and genre:
-          response = await generate_response(question, genre, prompt)
+          response = await generate_response(question, genre)
           st.write("Response:")
           st.write(response)
       else:
-          st.error("Please input all the required fields")
+          st.error("Please enter both fields.")
 
 #run the app
 if __name__ == "__main__":
